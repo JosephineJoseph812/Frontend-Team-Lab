4bfec47762fb2834c03edaff8322dbf0e92fd2fd
Josephine <josephinejoseph812@gmail.com>
2026-02-23 13:54:08 +0200

    Added features annimation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
-    alert("Welcome to Version 1 🚀");
+    const message = document.createElement("p");
+    message.textContent = "Welcome to Version 2 🚀";
+    message.style.color = "darkblue";
+    message.style.textAlign = "center";
+
+    document.body.appendChild(message);
 }
